0aa9b59e458930c82c181a5f6b9b9136c352c5da
innTall <troll999@hot.ee>
2020-10-03 20:26:00 +0200

    small fixed and chart

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 from flaskr.frames import get_candles
 from flaskr.frames import get_trades
 from flaskr.charts import candlestick_chart
+#from flaskr.tabel import tabel
 
 client = Client()
 
@@ -19,7 +20,7 @@ def get_dashboard():
   if request.method == 'POST':
     symbol = request.form['symbol']
     interval = request.form['interval']
-    candles = client.get_klines(symbol=symbol, intervals=intervals)
+    candles = client.get_klines(symbol=symbol, interval=interval)
     (gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu, sellu, nums, fd, ld, vma, vmb,
     vms, vbs, vav, dmb, dms, dbs) = get_candles(candles, unit=15)
     candle_chart = candlestick_chart(gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu,
@@ -27,10 +28,77 @@ def get_dashboard():
     return candle_chart
   else:
     symbol = request.args['symbol']
-    interval = '1h'
+    interval = '4h'
     candles = client.get_klines(symbol=symbol, interval=interval)
     (gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu, sellu, nums, fd, ld, vma, vmb,
     vms, vbs, vav, dmb, dms, dbs) = get_candles(candles, unit=15)
     candle_chart = candlestick_chart(gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu,
     sellu, nums, fd, ld, vma, vmb, vms, vbs, vav, dmb, dms, dbs)
     return render_template('dashboard.html', candle_chart=candle_chart, intervals=intervals)
+'''
+@app.route('/downblock', methods=['GET', 'POST'])
+def get_tabel():
+  intervals = ['1m', '5m', '15m', '1h', '4h', '1d', '3d', '1w', '1M']
+  if request.method == 'POST':
+    symbol = request.form['symbol']
+    interval = request.form['interval']
+    candles = client.get_klines(symbol=symbol, intervals=intervals)
+    (gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu, sellu, nums, fd, ld, vma, vmb,
+    vms, vbs, vav, dmb, dms, dbs) = get_candles(candles, unit=15)
+    tabel_volume = tabel(gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu,
+    sellu, nums, fd, ld, vma, vmb, vms, vbs, vav, dmb, dms, dbs)
+    return tabel_volume
+  else:
+    symbol = request.args['symbol']
+    interval = '1h'
+    candles = client.get_klines(symbol=symbol, interval=interval)
+    (gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu, sellu, nums, fd, ld, vma, vmb,
+    vms, vbs, vav, dmb, dms, dbs) = get_candles(candles, unit=15)
+    tabel_volume = tabel(gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu,
+    sellu, nums, fd, ld, vma, vmb, vms, vbs, vav, dmb, dms, dbs)
+    return render_template('downblock.html', tabel_volume=tabel_volume, intervals=intervals)
+
+@app.route('/tradeboard', methods=['GET', 'POST'])
+def get_tradeboard():
+  times = ['6h', '12h', '24d', '2d', '3d', '4d', '5d' + 'ago UTC']
+  if request.method == 'POST':
+    symbol = request.form['symbol']
+    time = request.form['time']
+    trades = client.aggregate_trade_iter(symbol=symbol, start_str=time)
+    (tot2, f8t, f11t, tot3, lp, mit, mat, avt, scat, buyd, selld, buyu, sellu, fd, ld, avb, mab,
+    avs, mas, absma, miav, vav, a15) = get_trades(trades, unit=15, period=15)
+    trade_chart = order_chart(tot2, f8t, f11t, tot3, lp, mit, mat, avt, scat, buyd, selld, buyu, sellu, fd, ld, avb, mab,
+    avs, mas, absma, miav, vav, a15)
+    return trade_chart
+  else:
+    symbol = 'request.args['symbol']'
+    time = '1h'
+    trades = client.aggregate_trade_iter(symbol=symbol, start_str='2 days ago UTC')
+    (tot2, f8t, f11t, tot3, lp, mit, mat, avt, scat, buyd, selld, buyu, sellu, fd, ld, avb, mab,
+    avs, mas, absma, miav, vav, a15) = get_trades(trades, unit=15, period=15)
+    trade_chart = order_chart(tot2, f8t, f11t, tot3, lp, mit, mat, avt, scat, buyd, selld, buyu, sellu, fd, ld, avb, mab,
+    avs, mas, absma, miav, vav, a15)
+    return render_template('tradeboard.html', trade_chart=trade_chart, times=times)
+
+@app.route('/upright', methods=['GET', 'POST'])
+def get_upright():
+  intervals = ['1m', '5m', '15m', '1h', '4h', '1d', '3d', '1w', '1M']
+  if request.method == 'POST':
+    symbol = request.form['symbol']
+    interval = request.form['interval']
+    candles = client.get_klines(symbol=symbol, intervals=intervals)
+    (gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu, sellu, nums, fd, ld, vma, vmb,
+    vms, vbs, vav, dmb, dms, dbs) = get_candles(candles, unit=15)
+    candle_chart = candlestick_chart(gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu,
+    sellu, nums, fd, ld, vma, vmb, vms, vbs, vav, dmb, dms, dbs)
+    return candle_chart
+  else:
+    symbol = 'BTCUSDT'
+    interval = '1h'
+    candles = client.get_klines(symbol=symbol, interval=interval)
+    (gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu, sellu, nums, fd, ld, vma, vmb,
+    vms, vbs, vav, dmb, dms, dbs) = get_candles(candles, unit=15)
+    candle_chart = candlestick_chart(gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu,
+    sellu, nums, fd, ld, vma, vmb, vms, vbs, vav, dmb, dms, dbs)
+    return render_template('upright.html', candle_chart=candle_chart, intervals=intervals)
+'''
--- flaskr/charts.py
+++ flaskr/charts.py
@@ -7,7 +7,6 @@ import plotly
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 import ta
-import math as m
 import json
 
 def candlestick_chart(gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu, sellu, nums,
@@ -18,7 +17,7 @@ fd, ld, vma, vmb, vms, vbs, vav, dmb, dms, dbs):
     vertical_spacing=0.02, # интервал по вертикали
     print_grid=True, # текстовое представление Subplot   {'rowspan': 2}
     specs=[[{'rowspan': 2}],
-          [None],
+          [None], 
           [{'secondary_y': True}],
           [{'secondary_y': True}]
     ]
@@ -36,30 +35,122 @@ fd, ld, vma, vmb, vms, vbs, vav, dmb, dms, dbs):
   y9 = gc['rsi'] #gc['rsi'] = ta.utils.dropna(gc['rsi'])
     
   gc = fig.add_trace(
-    go.Candlestick(
-      x=x,
-      open=y1,
-      high=y2,
-      low=y3,
-      close=y4
-    )
+    go.Candlestick(x=x, open=y1, high=y2, low=y3, close=y4, name='candles',
+                  line = dict(width=1),
+                  increasing = dict(
+                    line = dict(color = '#008000', width = 1)
+                  ),
+                  decreasing = dict(
+                    line = dict(color = '#FF0000', width = 1)
+                  )), row=1, col=1 #secondary_x=False                
+  )
+  '''
+  gc = fig.add_trace(go.Bar(x=y0, y=x0, name='volprice', orientation='h',
+                      marker = dict(
+                        line = dict(width = 1, color = '#6A5ACD')
+                      )), row=1, col=1, #secondary_x=True
+  )
+  '''
+  gc = fig.add_trace(go.Line(x=x, y=y5, name='volbuy',
+                        line = dict(width = 1, color = '#006400')
+                        ), row=3, col=1, secondary_y=False
+  )
+             
+  gc = fig.add_trace(go.Line(x=x, y=y6, name='volsell',
+                        line = dict(width = 1, color = '#FF0000')
+                        ), row=3, col=1, secondary_y=False
+  )
+          
+  gc = fig.add_trace(go.Bar(x=x, y=y7, name='delta%',
+                      marker = dict(
+                        line = dict(width = 1, color = '#6A5ACD')
+                      )), row=3, col=1, secondary_y=True
   )
   
-  gc = fig.add_trace(go.Scatter(
-        x=x, y=y5, name='volbuy'), row=3, col=1, secondary_y=False)
-         
-  gc = fig.add_trace(go.Scatter(
-        x=x, y=y6, name='volsell'), row=3, col=1, secondary_y=False)
-          
-  gc = fig.add_trace(go.Bar(
-        x=x, y=y7, name='delta%'), row=3, col=1, secondary_y=True)
-  
-  gc = fig.add_trace(go.Scatter(
-        x=x, y=y8, name='ao'), row=4, col=1, secondary_y=False)
-  
-  gc = fig.add_trace(go.Scatter(
-        x=x, y=y9, name='rsi'), row=4, col=1, secondary_y=True)
+  gc = fig.add_trace(go.Bar(x=x, y=y8, name='ao',
+                      marker = dict(
+                        line=dict(width = 0.8, color = '#228B22')
+                      )), row=4, col=1, secondary_y=False
+  )
 
+  gc = fig.add_trace(go.Line(x=x, y=y9, name='rsi',
+                        line = dict(width = 1, color = '#FF0000')
+                      ), row=4, col=1, secondary_y=True
+  )
+            
+  fig.update_layout(xaxis_rangeslider_visible=False,   #line_width=0.6
+                  paper_bgcolor='rgba(0,0,0,0)',
+                  plot_bgcolor='rgba(0,0,0,0)',
+                  shapes=[dict(type='line', xref='x1', yref='y1', line_width=0.8,
+                          x0=fd, y0=avc, x1=ld, y1=avc, line_color = '#A9A9A9',
+                          line = dict(dash='solid')),
+                        dict(type='line', xref='x1', yref='y1', line_width=0.8,
+                          x0=fd, y0=mic, x1=ld, y1=mic, line_color = '#A9A9A9',
+                          line = dict(dash='dot')),
+                        dict(type='line', xref='x1', yref='y1', line_width=0.8,
+                          x0=fd, y0=buyd, x1=ld, y1=buyd, line_color = '#A9A9A9',
+                          line = dict(dash='dot')),
+                        dict(type='line', xref='x1', yref='y1', line_width=0.8,
+                          x0=fd, y0=selld, x1=ld, y1=selld, line_color = '#A9A9A9',
+                          line = dict(dash='dot')),
+                        dict(type='line', xref='x1', yref='y1', line_width=0.8,
+                          x0=fd, y0=buyu, x1=ld, y1=buyu, line_color = '#A9A9A9',
+                          line = dict(dash='dot')),
+                        dict(type='line', xref='x1', yref='y1', line_width=0.8,
+                          x0=fd, y0=sellu, x1=ld, y1=sellu, line_color = '#A9A9A9',
+                          line = dict(dash='dot')),
+                        dict(type='line', xref='x1', yref='y1', line_width=0.8,
+                          x0=fd, y0=mac, x1=ld, y1=mac, line_color = '#A9A9A9',
+                          line = dict(dash='dot'))],
+                        #dict(type='line', xref='x4', yref='y4', name='rsi', line_width=0.8,
+                          #x0=fd, y0=30, x1=ld, y1=30, line_color='#A9A9A9',
+                          #line = dict(dash='dot')),
+                        #dict(type='line', xref='x4', yref='y4', name='rsi', line_width=0.8,
+                          #x0=fd, y0=70, x1=ld, y1=70, line_color='#A9A9A9',
+                          #line = dict(dash='dot'))],
+                  xaxis = dict(
+                    showgrid = False,
+                    showline = False,
+                    showticklabels = False,
+                    gridwidth = 1
+                  ),
+                  yaxis = dict(
+                    showgrid = False,
+                    showline = True,
+                    gridcolor = '#bdbdbd',
+                    gridwidth = 1,
+                    tickfont = dict(
+                      family = 'Old Standard TT, serif',
+                      size = 10,
+                      color = 'blue'
+                    )
+                  ), margin=dict(l=0, r=0, t=0, b=0)
+  )
+ 
+  graphJSON = json.dumps(gc, cls=plotly.utils.PlotlyJSONEncoder)
+  return graphJSON
+'''
+def order_chart(tot2, f8t, f11t, tot3, lp, mit, mat, avt, scat, buyd, selld, buyu, sellu, fd, ld, avb, mab,
+    avs, mas, absma, miav, vav, a15):
+
+  fig = make_subplots(rows=3, cols=1,
+    shared_xaxes=True, # связывание осей x
+    vertical_spacing=0.02, # интервал по вертикали
+    print_grid=True, # текстовое представление Subplot   {'rowspan': 2}
+    specs=[[{'rowspan': 2}],
+          [None],
+          [{'secondary_y': True}],
+    ]
+  )
+  
+  x = gc['date']
+  y1 = gc['price']
+  y2 = gc['buy']
+  y3 = gc['sell']
+  y4 = gc['ao'] #gc['ao'] = ta.utils.dropna(gc['ao'])
+  y5 = gc['rsi'] #gc['rsi'] = ta.utils.dropna(gc['rsi'])
+    
+  
   fig.update_layout(xaxis_rangeslider_visible=False,
                   xaxis = dict(
                     showgrid = False,
@@ -81,9 +172,29 @@ fd, ld, vma, vmb, vms, vbs, vav, dmb, dms, dbs):
                   margin=dict(l=0, r=0, t=0, b=0))
  
   graphJSON = json.dumps(gc, cls=plotly.utils.PlotlyJSONEncoder)
-  return graphJSON
+  return graphJSON  
 
-  
+def small_chart(gc, piv, lpc, mic, mac, avc, scac, buyd, selld, buyu, sellu, nums,
+fd, ld, vma, vmb, vms, vbs, vav, dmb, dms, dbs):
+
+  x = gc['date']
+  y1 = gc['open']
+  y2 = gc['high']
+  y3 = gc['low']
+  y4 = gc['close']
+
+trace = go.Candlestick(
+   x = x,
+   open = y1, 
+   high = y2,
+   low = y3, 
+   close = y4
+)
+fig.update_layout(xaxis_rangeslider_visible=False,
+                  margin=dict(l=0, r=0, t=0, b=0))
+ 
+  graphJSON = json.dumps(gc, cls=plotly.utils.PlotlyJSONEncoder)
+  return graphJSON
 
   #row_heights=[0.6, 0.2, 0.2], # относительная высота строк Subplot
   # Set y-axes titles
@@ -95,3 +206,5 @@ fd, ld, vma, vmb, vms, vbs, vav, dmb, dms, dbs):
    
 # py.image.save_as({'data':data}, 'scatter_plot', format='png')
 # py.image.save_as(fig, 'my_plot.png')
+'''
+
